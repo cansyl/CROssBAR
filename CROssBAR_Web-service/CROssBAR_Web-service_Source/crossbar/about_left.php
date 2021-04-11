ff4056e89fce9a9dbd6c283d07623efb9f530691
Tunca Dogan <tuncadogan@gmail.com>
2021-04-11 15:52:16 +0300

    Delete CROssBAR_Web-service/CROssBAR_Web-service_Source/crossbar directory

--- CROssBAR_Web-service/CROssBAR_Web-service_Source/crossbar/about_left.php
+++
@@ -1,7 +0,0 @@
-<nav class="nav flex-column">
-  <a class="nav-link active" href="#">CROssBAR Knowledge Graphs</a>
-  <a class="nav-link" href="#">Link</a>
-  <a class="nav-link" href="#">Link</a>
-  <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-</nav>
-</nav>
